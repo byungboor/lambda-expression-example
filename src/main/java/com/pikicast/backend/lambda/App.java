5b230c1184824b452108c4a14a1ab0de174faf29
Benjamin <benjamin@pikicast.com>
2016-02-23 15:40:18 +0900

    init

---
+++ src/main/java/com/pikicast/backend/lambda/App.java
@@ -0,0 +1,13 @@
+package com.pikicast.backend.lambda;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
